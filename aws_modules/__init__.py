35f3fc59a5381c0914698d56746dc9cc4f1a2e3a
Brandon Thacker <b.thacker@outlook.com>
2021-10-18 11:35:52 -0400

    exampel architecture

---
+++ aws_modules/__init__.py
@@ -0,0 +1,13 @@
+# import all
+from . import apigw
+from . import apigwv2
+from . import appsync  
+from . import ec2_public_ips
+from . import ecs
+from . import elbv2
+from . import lmbd
+from . import rds
+from . import route53_domains
+from . import route53
+from . import s3
+from . import session
---
+++ aws_modules/example_class_2.py
@@ -0,0 +1,15 @@
+import boto3
+
+
+class Example_Class_2:
+
+    example_variable = None
+
+    def __init__(self):
+        # variable /class initialization goes here etc
+        self.example_variable = 0
+        
+    def run_report(self):
+        pass
+        # report logic goes here
+
